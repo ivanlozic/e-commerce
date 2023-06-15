24251deb897d124101044fe09f12332f3b5a7ad8
Ivan Lozic <ivanlozic995@gmail.com>
2023-06-15 22:01:59 +0200

    ProductItemPage

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
+import { useDispatch } from 'react-redux'
+import { addItemToCart } from '../reduxStore/reducers/cartReducer'
 import Link from 'next/link'
-import { useDispatch } from 'react-redux';
-import { addItemToCart } from '../reduxStore/reducers/cartReducer';
 
-
-const IMAGE_SIZE = 300
+const IMAGE_SIZE = 400
 
 export default function ProductItemPage() {
+  const [imageSrc, setImageSrc] = useState('/assets/images/1.jpg')
+  const productId = 1
+  const dispatch = useDispatch()
 
-  const productId = 1;
-  const dispatch = useDispatch();
+
+  const smallImageUrls = [
+    '/assets/images/1.jpg',
+    '/assets/images/trash.png',
+    '/assets/images/1.jpg',
+    '/assets/images/1.jpg'
+  ]
+
+  const handleSmallImageClick = (imageSrc: string) => {
+    setImageSrc(imageSrc)
+  }
+
 
   const handleAddToCart = () => {
     const product = {
@@ -18,33 +30,47 @@ export default function ProductItemPage() {
       name: 'Product name',
       description: 'dasdas',
       price: 36,
-      quantity:1,
+      quantity: 1,
       image: '/assets/images/1.jpg'
-    };
+    }
 
-    dispatch(addItemToCart(product));
-  };
+    dispatch(addItemToCart(product))
+  }
+
   return (
-    <div className='productContainer'>
-      <h2>Product name</h2>
-      <Link href='/products/0'>
+    <div className='productPageContainer'>
+      <Link href={'/products'} className='productPageContainer__link'>Back to products</Link> 
+      <div className='productPageContainer_img-container'>
+      {smallImageUrls.map((imageUrl, index) => (
+          <Image
+            key={index}
+            width={70}
+            height={70}
+            className='productPageContainer__img-s'
+            src={imageUrl}
+            alt={`Small image ${index}`}
+            onClick={() => handleSmallImageClick(imageUrl)}
+          />
+        ))}
+      </div>
       <Image
-        className='productContainer__img'
-        src='/assets/images/1.jpg'
+        className='productPageContainer__img'
+        src={imageSrc}
         alt=''
         width={IMAGE_SIZE}
         height={IMAGE_SIZE}
       />
-      </Link>
-   
-      <p>
-        Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Fugit earum deserunt quod, natus debitis tempora? Sunt illum quos
-        aspernatur inventore.
-      </p>
-      <div className='productContainer__buttonBox'>
-        <p>Price: 36e</p>
-        <button onClick={handleAddToCart}>Add to cart</button>
+      <div>
+        <h2>Product name</h2>
+        <p>
+          Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Fugit earum deserunt quod, natus debitis tempora? Sunt illum quos
+          aspernatur inventore.
+        </p>
+        <div className='productPageContainer__buttonBox'>
+          <p>Price: 36e</p>
+          <button onClick={handleAddToCart}>Add to cart</button>
+        </div>
       </div>
     </div>
   )
