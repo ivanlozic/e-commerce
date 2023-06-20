38eba9fd5d8df87cd935f9d26810ef52606e37cd
Ivan Lozic <ivanlozic995@gmail.com>
2023-06-20 11:29:08 +0200

    Product quantity logic

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { useDispatch } from 'react-redux'
-import { addItemToCart } from '../reduxStore/reducers/cartReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  addItemToCart,
+  decreaseItemQuantity,
+  increaseItemQuantity
+} from '../reduxStore/reducers/cartReducer'
 import Link from 'next/link'
+import { RootState } from '../reduxStore/store'
 
 const IMAGE_SIZE = 400
 
 export default function ProductItemPage() {
   const [imageSrc, setImageSrc] = useState('/assets/images/1.jpg')
+  const [addToCartClicked, setAddToCartClicked] = useState(false)
+  const cartItems = useSelector((state: RootState) => state.cart)
+  const [quantity, setQuantity] = useState(0)
   const productId = 1
   const dispatch = useDispatch()
 
+  useEffect(() => {
+    const cartItem = cartItems.find((item: any) => item.id === productId)
+    if (cartItem) {
+      setQuantity(cartItem.quantity)
+    }
+  }, [cartItems, productId])
+
+  const handleDecreaseQuantity = () => {
+    dispatch(decreaseItemQuantity(productId))
+  }
+
+  const handleIncreaseQuantity = () => {
+    dispatch(increaseItemQuantity(productId))
+  }
 
   const smallImageUrls = [
     '/assets/images/1.jpg',
@@ -23,7 +45,6 @@ export default function ProductItemPage() {
     setImageSrc(imageSrc)
   }
 
-
   const handleAddToCart = () => {
     const product = {
       id: productId,
@@ -35,13 +56,21 @@ export default function ProductItemPage() {
     }
 
     dispatch(addItemToCart(product))
+
+    setAddToCartClicked(true)
+
+    setTimeout(() => {
+      setAddToCartClicked(false)
+    }, 100)
   }
 
   return (
     <div className='productPageContainer'>
-      <Link href={'/products'} className='productPageContainer__link'>Back to products</Link> 
+      <Link href={'/products'} className='productPageContainer__link'>
+        Back to products
+      </Link>
       <div className='productPageContainer_img-container'>
-      {smallImageUrls.map((imageUrl, index) => (
+        {smallImageUrls.map((imageUrl, index) => (
           <Image
             key={index}
             width={70}
@@ -60,7 +89,7 @@ export default function ProductItemPage() {
         width={IMAGE_SIZE}
         height={IMAGE_SIZE}
       />
-      <div>
+      <div className='productPageContainer__textContainer'>
         <h2>Product name</h2>
         <p>
           Description: Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -69,7 +98,20 @@ export default function ProductItemPage() {
         </p>
         <div className='productPageContainer__buttonBox'>
           <p>Price: 36e</p>
-          <button onClick={handleAddToCart}>Add to cart</button>
+          <button
+            className={
+              addToCartClicked ? 'productPageContainer__buttonBox-clicked' : ''
+            }
+            onClick={handleAddToCart}
+          >
+            Add to cart
+          </button>
+
+          <div className='productPageContainer__buttonBox-quantity'>
+            <button onClick={handleDecreaseQuantity}>-</button>
+            <p>{quantity}</p>
+            <button onClick={handleIncreaseQuantity}>+</button>
+          </div>
         </div>
       </div>
     </div>
